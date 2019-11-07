52346a78f55b0f11e51186d3049dc3209af43fff
Student <student@donation.local>
2019-11-07 15:21:49 -0600

    Class Django practice

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from app.models import Coffee, Transaction
+
+
+@admin.register(Coffee)
+class CoffeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Coffee(models.Model):
+    name = models.TextField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    time = models.DateTimeField()
+    item = models.ForeignKey(Coffee, on_delete=models.CASCADE)
+    pre_tax = models.FloatField()
+    tax = models.FloatField()
